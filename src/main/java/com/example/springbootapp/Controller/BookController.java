582851d9124120e0d3d3585b3999807d0ac23b51
subhrajeet <subhrajeet.ankhi@gmail.com>
2020-12-10 10:20:33 +0530

    some

--- src/main/java/com/example/springbootapp/Controller/BookController.java
+++ src/main/java/com/example/springbootapp/Controller/BookController.java
@@ -47,7 +47,6 @@ public class BookController {
         final Book updatedBook = bookrepository.save(book);
         return ResponseEntity.ok(updatedBook);
     }
-    //hbyuhbuinbiuhbyu uy
     @DeleteMapping("/postgres/{Id}")
     public Map<String, Boolean> deleteBook(@PathVariable(value = "Id") int Id)
             throws ResourceNotFoundException {
