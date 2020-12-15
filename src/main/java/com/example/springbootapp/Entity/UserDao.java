8276f5b9cd19bcafded5e6744bd8403149110928
subhrajeet <subhrajeet.ankhi@gmail.com>
2020-12-15 16:07:46 +0530

    solve an issue

--- src/main/java/com/example/springbootapp/Entity/UserDao.java
+++ src/main/java/com/example/springbootapp/Entity/UserDao.java
@@ -1,13 +1,16 @@
 package com.example.springbootapp.Entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+import lombok.ToString;
 
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
-
+@Data
 @Entity
-@Table(name = "user")
+@ToString
+@Table(name = "Usertab")
 public class UserDao {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,9 +20,9 @@ public class UserDao {
     @Column
     @JsonIgnore
     private String password;
-    @OneToMany(targetEntity = Book.class,cascade = CascadeType.ALL)
+    /*@OneToMany(targetEntity = Book.class,cascade = CascadeType.ALL)
     @JoinColumn(name ="cp_fk",referencedColumnName = "user_id")
-    List<Book> book = new ArrayList<>();
+    List<Book> book = new ArrayList<>();*/
     public String getUsername() {
         return username;
     }
